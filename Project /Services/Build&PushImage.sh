d9bf10d1ecf722e608fcc359f58e4306054e57ce
shishirv133 <140936916+shishirv133@users.noreply.github.com>
2023-11-11 21:16:36 +0530

    Create Build&PushImage.sh

---
+++ Project /Services/Build&PushImage.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Dockerfile location  for the microservices
+ORDER_SERVICE_REPO="https://${GITHUB_TOKEN}@raw.githubusercontent.com/${GIT_USER_NAME}/${GIT_REPO_NAME}/main/OrderService/Dockerfile"
+PRODUCT_SERVICE_REPO="https://${GITHUB_TOKEN}@raw.githubusercontent.com/${GIT_USER_NAME}/${GIT_REPO_NAME}/main/ProductService/Dockerfile"
+CUSTOMER_SERVICE_REPO="https://${GITHUB_TOKEN}@raw.githubusercontent.com/${GIT_USER_NAME}/${GIT_REPO_NAME}/main/CustomerService/Dockerfile"
+
+# Function to build a microservice
+build_microservice() {
+    local dockerfile_url="$1"
+    local service_name="$2"
+
+    # Download the Dockerfile
+    echo "Downloading Dockerfile for $service_name..."
+    curl -sSLJO $dockerfile_url || { echo "Error downloading Dockerfile for $service_name"; exit 1; }
+
+    # Build the microservice
+    echo "Building $service_name..."
+    docker build -t ${DOCKER_USERNAME}/${service_name}:${BUILD_NUMBER} -f Dockerfile . || { echo "Error building $service_name"; exit 1; }
+    docker push ${DOCKER_USERNAME}/${service_name}:${BUILD_NUMBER} || { echo "Error pushing Docker image for $service_name"; exit 1; }
+    rm Dockerfile
+
+    echo "Build process completed for $service_name."
+}
+
+# Build OrderService
+build_microservice "$ORDER_SERVICE_REPO" "OrderService"
+
+# Build ProductService
+build_microservice "$PRODUCT_SERVICE_REPO" "ProductService"
+
+# Build CustomerService
+build_microservice "$CUSTOMER_SERVICE_REPO" "CustomerService"
+
+echo "Build process completed for all microservices."
