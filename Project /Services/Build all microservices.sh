41d685aa968d7b38eb869c95d2b102d956672813
shishirv133 <140936916+shishirv133@users.noreply.github.com>
2023-11-11 21:06:12 +0530

    Update Build all microservices.sh

--- Project /Services/Build all microservices.sh
+++ Project /Services/Build all microservices.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
 # GitHub repositories for the microservices
-ORDER_SERVICE_REPO="https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}//${GIT_REPO_NAME}/OrderService.git"
-PRODUCT_SERVICE_REPO="https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}//${GIT_REPO_NAME}/ProductService.git"
-CUSTOMER_SERVICE_REPO="https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}//${GIT_REPO_NAME}/CustomerService.git"
+ORDER_SERVICE_REPO="https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}/${GIT_REPO_NAME}/OrderService.git"
+PRODUCT_SERVICE_REPO="https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}/${GIT_REPO_NAME}/ProductService.git"
+CUSTOMER_SERVICE_REPO="https://${GITHUB_TOKEN}@github.com/${GIT_USER_NAME}/${GIT_REPO_NAME}/CustomerService.git"
+
+# Function to clone and build a microservice
+clone_and_build() {
+    local repo_url="$1"
+    local service_name="$2"
+
+    # Clone the repository
+    echo "Cloning $service_name..."
+    git clone $repo_url || { echo "Error cloning $service_name repository"; exit 1; }
+
+    # Build the microservice
+    echo "Building $service_name..."
+    cd $service_name
+    mvn clean package || { echo "Error building $service_name"; exit 1; }
+    cd ..
+
+    echo "Build process completed for $service_name."
+}
 
 # Clone and build OrderService
-echo "Building OrderService..."
-git clone $ORDER_SERVICE_REPO
-cd OrderService
-mvn clean package
-cd ..
+clone_and_build "$ORDER_SERVICE_REPO" "OrderService"
 
 # Clone and build ProductService
-echo "Building ProductService..."
-git clone $PRODUCT_SERVICE_REPO
-cd ProductService
-mvn clean package
-cd ..
+clone_and_build "$PRODUCT_SERVICE_REPO" "ProductService"
 
 # Clone and build CustomerService
-echo "Building CustomerService..."
-git clone $CUSTOMER_SERVICE_REPO
-cd CustomerService
-mvn clean package
-cd ..
+clone_and_build "$CUSTOMER_SERVICE_REPO" "CustomerService"
 
 # Cleanup: Remove the cloned repositories
 echo "Cleaning up..."
 rm -rf OrderService ProductService CustomerService
-
 
 echo "Build process completed for all microservices."
